701dcb5bf7ff2ee8222002d442b62b3c62f268c6
suada <suadaljuaid1@gmail.com>
2025-11-17 09:40:59 +0300

    Lab3 - Arrays-Part1

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -1,8 +1,35 @@
 package Arrays;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("test2");
+
+        int []numbers={1,2,3,4,5};
+       // int []numbers2= new int[5];
+      //  numbers2[0]=1;
+
+        for(int i =0 ; i<numbers.length;i++)
+            System.out.println("Index["+i+"]"+" = "+numbers[i]);
+
+        for(int num :numbers)
+            System.out.println(num);
+
+
+
+
+
+
+
+
+       // System.out.println(Arrays.toString(numbers2));
+       // System.out.println(Arrays.toString(numbers));
+
+
+
+
+
+
     }
+
 }
